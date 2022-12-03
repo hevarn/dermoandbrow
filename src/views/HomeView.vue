4bbf941e3dcb366293498c9b39fc6ee12dd5c39e
hevarne <nunzio.moderni@gmail.com>
2022-12-03 09:56:16 +0100

    develop make started branch

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import FooterComponent from '@/components/FooterComponent/footerComponent.vue'
+</script>
+
+<template>
+  <main>
+    <footer-component />
+  </main>
+</template>
