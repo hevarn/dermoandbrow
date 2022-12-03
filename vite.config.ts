5a737579b45877f5f98ae777720ea33e68fae21c
hevarne <nunzio.moderni@gmail.com>
2022-12-03 11:03:11 +0100

    [PRT-01] add config file and asset picture and font

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    host: true,
+    port: 8001,
+    watch: {
+      usePolling: true
+    }
+  }
 });
